d2c9eba598d220c9b49a0396ef2910bf1f94a4b5
BuildTools <vreede04@gmail.com>
2016-07-03 19:21:02 +0200

    Fixed the punch ability

--- src/hypixel/events/interact_events.java
+++ src/hypixel/events/interact_events.java
@@ -6,6 +6,10 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.player.PlayerInteractAtEntityEvent;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.Vector;
 
@@ -22,14 +26,19 @@ public class interact_events implements Listener {
     @EventHandler
     public void onPunch(EntityDamageByEntityEvent event) {
         final Player puncher = (Player) event.getDamager();
+        event.setCancelled(true);
+        if(!(event.getEntity() instanceof Player)) {
+            return;
+        }
         Player target = (Player) event.getEntity();
         if(!puncher.hasPermission("punch") || !target.hasPermission("staff")) {
             return;
         }
         if(coolDown.containsKey(puncher)) {
-            puncher.sendMessage("§cYou punch ability is in cooldown!");
+            puncher.sendMessage("§cYour punch ability is in cooldown!");
             return;
         }
+        coolDown.put(puncher, 30);
         target.setVelocity(new Vector(0, 5, 0));
         Bukkit.broadcastMessage(puncher.getDisplayName() + "§7has punched §9" + target.getName() + "§7into the sky!");
         new BukkitRunnable() {
