44ff517ac188ce10e67e38ef0938d49fd1d4aa0b
BuildTools <vreede04@gmail.com>
2016-07-03 19:30:42 +0200

    Added anti-fall damage

--- src/hypixel/events/damage_listener.java
+++ src/hypixel/events/damage_listener.java
@@ -4,14 +4,23 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
 
 /**
  * Created by Niels on 03-07-16.
  */
+
 public class damage_listener implements Listener {
 
     @EventHandler
     public void onDameg(EntityDamageByEntityEvent event) {
+        if(event.getEntity() instanceof Player) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onDamageTake(EntityDamageEvent event) {
         if(event.getEntity() instanceof Player) {
             event.setCancelled(true);
         }
